a20757980cb60d8c99333fe22ff49668d31ad19a
ramamimu <ahhmaad.blogger@gmail.com>
2022-10-25 12:35:12 +0700

    feat: soal 3, 4, dan 5

--- Garden/script.sh
+++ Garden/script.sh
@@ -2,5 +2,11 @@ echo nameserver 10.10.3.2 > /etc/resolv.conf
 echo nameserver 10.10.2.2 >> /etc/resolv.conf
 echo nameserver 192.168.122.1 >> /etc/resolv.conf
 
+apt-get update
+wait
+
 apt-get install lynx -y
+wait
+
+apt-get install dnsutils
 wait
--- WISE/script.sh
+++ WISE/script.sh
@@ -12,4 +12,7 @@ mkdir /etc/bind/jarkom
 cp /etc/bind/db.local /etc/bind/jarkom/c01.com
 cat /root/conf_bind_c01.txt > /etc/bind/jarkom/c01.com
 
+cp /etc/bind/db.local /etc/bind/jarkom/2.10.10.in-addr.arpa
+cat /root/conf_reverse_domain.txt > /etc/bind/jarkom/2.10.10.in-addr.arpa
+
 service bind9 start
