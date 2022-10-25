2f3f4c6dd6d49ddcbe9267a706310d4dca189864
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2022-10-26 01:48:51 +0700

    download drive file

--- Eden/script.sh
+++ Eden/script.sh
@@ -6,4 +6,10 @@ wait
 apt-get install apache2 -y
 wait
 
+apt-get install wget -y
+apt-get install zip -y
+
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1S0XhL9ViYN7TyCj2W66BNEXQD2AAAw2e' -O wise.c01.com.zip
+unzip wise.c01.com.zip -d /var/www/
+
 service apache2 start
