a17b0b4932c862db2d0b5a2bfbf8740238fdd13e
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2022-10-26 15:58:58 +0700

    no 8 no 9

--- Eden/script.sh
+++ Eden/script.sh
@@ -6,10 +6,26 @@ wait
 apt-get install apache2 -y
 wait
 
+apt-get install libapache2-mod-php7.0 -y
+
 apt-get install wget -y
+apt-get install php -y
 apt-get install zip -y
 
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1S0XhL9ViYN7TyCj2W66BNEXQD2AAAw2e' -O wise.c01.com.zip
-unzip wise.c01.com.zip -d /var/www/
+if [ ! -d "/var/www/wise.c01.com/" ]
+then
+    wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1S0XhL9ViYN7TyCj2W66BNEXQD2AAAw2e' -O wise.c01.com.zip
+    unzip wise.c01.com.zip -d /var/www/
+    mv /var/www/wise /var/www/wise.c01.com
+fi
+
+cat conf_apache.txt > /etc/apache2/sites-available/000-default.conf
+
+touch /var/www/wise.c01.com/.htaccess
+
+cat htaccess.txt > /var/www/wise.c01.com/.htaccess
+
+
+a2enmod rewrite
 
 service apache2 start
